97f779ea34cd7cbd8897de37858263da7d5664d9
Thomas Lauer <tjlauer36579@gmail.com>
2021-09-21 22:16:17 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Thomas Lauer
+// September 21, 2021
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_20210921
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shirt aShirt1 = new Shirt("t-shirt", "red", "XXXXS", 4.20);
+            Shirt aShirt2 = new Shirt("v-neck", "purple", "M", 6.9);
+            Shirt aShirt3 = new Shirt("turtle neck", "brown", "XXXXL", 69420);
+            Shirt aShirt4 = new Shirt("croptop", "tie-dye", "U", 0);
+            Shirt aShirt5 = new Shirt("berriesandcream", "burgundy", "XL", 666);
+
+            aShirt3.ApplyDiscount(0.69);
+
+            List<Shirt> listOfShirts = new List<Shirt>();
+            listOfShirts.Add(aShirt1);
+            listOfShirts.Add(aShirt2);
+            listOfShirts.Add(aShirt3);
+            listOfShirts.Add(aShirt4);
+            listOfShirts.Add(aShirt5);
+
+            foreach(Shirt s in listOfShirts)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shirt.cs
@@ -0,0 +1,81 @@
+﻿// Thomas Lauer
+// September 21, 2021
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_20210921
+{
+    public class Shirt
+    {
+        // Variables
+        private int id = -1;
+        private string name = "n/a";
+        private string color = "n/a";
+        private string size = "n/a";
+        private double price = 999999999999999999.9;
+        
+        // Properties
+        public int ID
+        {
+            get { return this.id;  }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public string Size
+        {
+            get { return this.size; }
+            set { this.size = value; }
+        }
+
+        public double Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+        // Constructors
+        public Shirt() : this("n/a", "n/a", "n/a", 999999999999999999.9) { }
+
+        public Shirt(string aName, string aColor, string aSize, double aPrice)
+        {
+            this.Name = aName;
+            this.Color = aColor;
+            this.Size = aSize;
+            this.Price = aPrice;
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            string msg = "";
+            msg += "ID: " + this.ID + "\n";
+            msg += "Name: " + this.Name + "\n";
+            msg += "Color: " + this.Color + "\n";
+            msg += "Size: " + this.Size + "\n";
+            msg += "Price: " + this.Price + "\n";
+            return msg;
+        }
+
+        public void ApplyDiscount(double perc)
+        {
+            this.Price = this.Price * (1 - perc);
+        }
+
+
+    }
+}
